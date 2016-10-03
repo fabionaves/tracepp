33ea2f89982cce6f1b5e7c8aba82da8c24d5da52
fabionaves <fabionaves@gmail.com>
2016-10-03 16:18:55 -0300

    New changelist

---
+++ main/components/git.py
@@ -0,0 +1,24 @@
+import git, os, shutil
+from django.conf import settings
+
+from main.models import Project
+
+
+def pull(project: Project):
+    if project.id:
+        DIR_NAME = os.path.join(settings.REPOSITORY_DIR, str(project.id))
+    else:
+        return False
+
+    if project.repository_url:
+        REMOTE_URL = project.repository_url
+    else:
+        return False
+
+    if not os.path.isdir(DIR_NAME):
+        os.mkdir(DIR_NAME)
+
+    repo = git.Repo.init(DIR_NAME)
+    origin = repo.create_remote('origin', REMOTE_URL)
+    origin.fetch()
+    origin.pull(origin.refs[0].remote_head)
