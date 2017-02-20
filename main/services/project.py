5fd8db8793827a069925438273c7c4423ab8ecb9
fabionaves <fabionaves@ipora-tifab.ifgoiano.local>
2017-02-20 09:24:27 -0300

    Grafico tela inicial

--- main/services/project.py
+++ main/services/project.py
@@ -2,6 +2,7 @@ from main.models import Artifact
 from main.models import Project, Requeriment
 from django.shortcuts import  get_object_or_404
 
+from main.models import Sprint
 from main.models import UserStory
 
 
@@ -24,6 +25,27 @@ class ProjectService:
     @staticmethod
     def get_project(project_id):
         return get_object_or_404(Project, pk=project_id)
+
+    @staticmethod
+    def get_num_sprints_from_project(project_id):
+        return Sprint.objects.filter(project=project_id).count()
+
+    @staticmethod
+    def get_num_requeriments_from_project(project_id):
+        return Requeriment.objects.filter(project=project_id).count()
+
+    @staticmethod
+    def get_num_userstories_from_project(project_id):
+        return UserStory.objects.filter(project=project_id).count()
+
+    @staticmethod
+    def get_num_artifacts_from_project(project_id):
+        return Artifact.objects.filter(
+            project=project_id,
+            requeriment__isnull=True,
+            sprint__isnull=True,
+            userstory__isnull=True,
+        ).count()
 
     @staticmethod
     def get_project_requeriments(project_id):
--- main/views/home_views.py
+++ main/views/home_views.py
@@ -14,16 +14,10 @@ class HomeView(TemplateViewProjectFilter):
 
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
-        context['total_sprints'] = Sprint.objects.filter(project=self.request.session.get('project_id', None)).count()
-        context['total_requeriments'] = Requeriment.objects.filter(project=self.request.session.get('project_id', None)).count()
+        context['total_sprints'] = ProjectService.get_num_sprints_from_project(self.request.session.get('project_id', None))
+        context['total_requeriments'] = ProjectService.get_num_requeriments_from_project(self.request.session.get('project_id', None))
         context['task_effort'] = UserStoryService.get_task_effor_per_userstory(self.request.session.get('project_id', None))
         context['storypoints_variation'] = ProjectService.get_userstories(self.request.session.get('project_id', None))
-        context['total_userstories'] = UserStory.objects.filter(
-            project=self.request.session.get('project_id', None)).count()
-        context['total_artifacts'] = Artifact.objects.filter(
-            project=self.request.session.get('project_id', None),
-            requeriment__isnull=True,
-            sprint__isnull=True,
-            userstory__isnull=True,
-        ).count()
+        context['total_userstories'] = ProjectService.get_num_userstories_from_project(self.request.session.get('project_id', None))
+        context['total_artifacts'] = ProjectService.get_num_artifacts_from_project(self.request.session.get('project_id', None))
         return context
