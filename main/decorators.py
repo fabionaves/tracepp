ff5eb45b037eba8efd3db39913925136136e22a2
fabionaves <fabionaves@ipora-tifab.ifgoiano.local>
2017-04-13 14:27:06 -0300

    correcao do redirecionamento do decorator url project

--- main/decorators.py
+++ main/decorators.py
@@ -1,4 +1,5 @@
 from django.shortcuts import redirect
+from django.urls import reverse
 from django.utils.decorators import available_attrs
 from django.utils.six import wraps
 
@@ -10,7 +11,7 @@ def require_project():
     def decorator(func):
         def inner(request, *args, **kwargs):
             if not 'project_id' in request.session:
-               return redirect('/project/')
+               return redirect(reverse('main:project'))
             return func(request, *args, **kwargs)
         return wraps(func, assigned=available_attrs(func))(inner)
     return decorator
