284a9c9f7c85c21a76efe0a383a6615ddaf5cdf8
fabionaves <fabionaves@ipora-tifab.ifgoiano.local>
2017-05-09 13:56:19 -0300

    erro no cadastro de usertory titulo grande

--- main/models/userstory.py
+++ main/models/userstory.py
@@ -11,7 +11,7 @@ from .project import Project
 class UserStory(models.Model,MyModel):
     code = models.CharField(_('Code'), max_length=30)
     reference = models.IntegerField(_('Bug Track Id'), null=True, blank=True)
-    title = models.CharField(_('Title'), max_length=50)
+    title = models.CharField(_('Title'), max_length=100)
     description = models.TextField(_('Description'),null=True, blank=True)
     acceptanceCriteria = models.TextField(_('Acceptance Criteria'),null=True, blank=True)
     requeriment = models.ManyToManyField(Requeriment)
